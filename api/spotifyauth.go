873b60f520e3ddaaebb6e03130568a36c9ebf714
Jack Saller <jack.saller@vasion.com>
2025-02-02 17:07:07 -0700

    Revert "callback url to vercel app"
    This reverts commit adfd90847c79dbe72613aeb291df2e3ea99c8acf.

--- api/spotifyauth.go
+++ api/spotifyauth.go
@@ -17,7 +17,7 @@ import (
 	sa "github.com/zmb3/spotify/v2/auth"
 )
 
-const redirectURI = "http://spotiterm.vercel.app/api/callback"
+const redirectURI = "http://localhost:8080/callback"
 
 // redirectURI is the OAuth redirect URI for the application.
 // You must register an application at Spotify's developer portal
